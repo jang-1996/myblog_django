a3352dd0bb249e48fdc60368f19ec696ae4b4fdd
altu-1996 <altu1996@naver.com>
2020-07-20 22:34:37 +0900

    HW

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import new, create, main, show
+from .views import new, create, main, show, update, delete
 
 app_name = "posts" 
 
@@ -8,4 +8,6 @@ urlpatterns = [
     path('create/', create, name="create"),
     path('', main, name="main"),
     path('/show/<int:id>', show, name="show"),
+    path('update/<int:id>/', update, name="update"),
+    path('delete/<int:id>/', delete, name="delete"),
     ]
